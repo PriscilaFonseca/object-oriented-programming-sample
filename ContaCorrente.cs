0d47d22ca36139f1251b26a4b0b28c18fd5f06fe
Priscila Fonseca <pripof@gmail.com>
2020-09-28 19:10:43 -0300

    ContaCorrente class with basic methods.

---
+++ ContaCorrente.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace CaixaEletronico
+{
+    class ContaCorrente
+    {
+        private double Saldo;
+        public string Titular;
+
+        public ContaCorrente(double valor)
+        {
+            this.Saldo = valor;
+        }
+
+        public double Saque(double valor)
+        {
+            if (Saldo >= valor)
+            {
+                return this.Saldo -= valor;
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente");
+                return this.Saldo;
+            }
+
+        }
+        public double Deposito(double valor)
+        {
+            return this.Saldo += valor;
+        }
+
+        public double ConsultarSaldo()
+        {
+            return this.Saldo;
+        }
+    }
+}
